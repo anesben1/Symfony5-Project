f237c7255cb02d4a9e679137655f820c95153b3b
anesben1 <anas.benhmidalakhal@esprit.tn>
2021-04-28 13:38:45 +0200

    register
     - register form + controller
     - redirect to loginpqge after registrqtion user of( GuardAuthenticatorHandler)
    instead of render normal page.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Security\LoginFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -42,4 +49,40 @@ class SecurityController extends AbstractController
     {
         throw new \Exception('Will be intercepted before getting here');
     }
+
+     /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(HttpFoundationRequest $request, UserPasswordEncoderInterface $passwordEncode, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $formAuthenticator)
+    {
+            if($request->isMethod('POST')){
+                $user = new User() ; 
+                $user->setEmail($request->request->get('email'));
+                $user->setFirstname('Mystery');
+                $user->setPassword($passwordEncode->encodePassword(
+                    $user,
+                    $request->request->get('password')
+                ));
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+
+            
+
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $formAuthenticator,
+                'main'
+
+            );
+
+        }
+
+        return $this->render('security/register.html.twig');
+
+    }
+       
+
 }
