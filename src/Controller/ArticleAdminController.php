9d7f663bfa59cf7d8bb7899e7797eb162c61c2ba
anesben1 <anas.benhmidalakhal@esprit.tn>
2021-05-05 05:22:32 +0200

    chapter done
    - create voter ( two functions ) :
      *support ()
      * OnVoteAuthenticate()
    - customize our  controller isGranted annotation !!
    - and we are done !!!! voter created

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -30,13 +30,12 @@ class ArticleAdminController extends AbstractController
 
      /**
      * @Route("/admin/article/{id}/edit")
+     * @IsGranted("MANAGE", subject="article")
      */
     public function edit(Article $article)
     {
-        if (!$this->isGranted('MANAGE', $article)) 
-        {
-
-        }
+        
+        
         dd($article);
 
     } 
--- src/Security/Voter/ArticleVoter.php
+++ src/Security/Voter/ArticleVoter.php
@@ -2,22 +2,34 @@
 
 namespace App\Security\Voter;
 
+use App\Entity\Article;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class ArticleVoter extends Voter
 {
+    private $security; 
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+        
+    }
+
+
     protected function supports($attribute, $subject)
     {
         // replace with your own logic
         // https://symfony.com/doc/current/security/voters.html
-        return in_array($attribute, ['POST_EDIT', 'POST_VIEW'])
+        return in_array($attribute, ['MANAGE'])
             && $subject instanceof \App\Entity\Article;
     }
 
     protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
     {
+        /** @var Article $subject */
         $user = $token->getUser();
         // if the user is anonymous, do not grant access
         if (!$user instanceof UserInterface) {
@@ -25,15 +37,21 @@ class ArticleVoter extends Voter
         }
 
         // ... (check conditions and return true to grant permission) ...
-        switch ($attribute) {
-            case 'POST_EDIT':
-                // logic to determine if the user can EDIT
-                // return true or false
-                break;
-            case 'POST_VIEW':
-                // logic to determine if the user can VIEW
-                // return true or false
-                break;
+        switch ($attribute) 
+        {
+            case 'MANAGE':
+                if ($subject->getAuthor() == $user){
+                    return true;
+                }
+
+                if ($this->security->isGranted('ROLE_ADMIN_ARTICLE')){
+                    return true;
+                }
+                
+                return false;
+        
+               
+         
         }
 
         return false;
