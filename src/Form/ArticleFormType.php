0dc7c619dedd9d740b37f51dbd9018ef2f79dc00
anesben1 <anas.benhmidalakhal@esprit.tn>
2021-05-06 21:58:42 +0200

    datetime
     - add a facier datetime with calender (not all browser can handle it )

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -3,7 +3,10 @@
 namespace App\Form ;
 
 use App\Entity\Article;
+
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,8 +17,14 @@ public function buildForm(FormBuilderInterface $builder, array $options)
 {
 
     $builder
-    ->add('title')
+    ->add('title', TextareaType::class , [
+        'help' => 'choose something catchy!'
+    ])
+    
     ->add('content')
+    ->add('publishedAt', null, [
+        'widget' => 'single_text'
+    ])
     ;
 
 }
