dff95f368e6c4338d6d9e9b27d7141443a08cde1
anesben1 <anas.benhmidalakhal@esprit.tn>
2021-05-24 19:53:57 +0200

    data transformer
     -create a datatransformer to change the textype into object while rendering and vice versa
     - customiwe the error resolver

---
+++ src/Form/UserSelectTextType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Form\DataTransformer\EmailToUserRraansformer;
+use App\Form\DataTransformer\EmailToUserTransformer;
+use App\Repository\UserRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserSelectTextType extends AbstractType
+{
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository; 
+        
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addModelTransformer(new EmailToUserTransformer($this->userRepository));
+    }
+
+    public function getParent()
+    {
+        return TextType::class;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'invalid_message' => ' Hmm, user not found!' , 
+        ]);
+        
+    }
+}
+
+
