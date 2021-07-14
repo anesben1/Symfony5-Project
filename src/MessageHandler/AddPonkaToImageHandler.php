04bb71e617b434b3e92f2cd64e0d9e4c8a86dcf8
anesben1 <anas.benhmidalakhal@esprit.tn>
2021-07-14 19:34:46 +0200

    begin messenger ponka
    first trial messenger

---
+++ src/MessageHandler/AddPonkaToImageHandler.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace App\MessageHandler;
+
+use App\Message\AddPonkaToImage;
+
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+
+
+class AddPonkaToImageHandler  implements MessageHandlerInterface
+{
+    public function __invoke(AddPonkaToImage  $addPonkaToImage )
+    {
+        dump($addPonkaToImage);
+    }
+
+}
+
+
